a9864f10d36a1ea3e657fc751860ce30dac446f9
OSMI <63618245+OSMINOZH@users.noreply.github.com>
2023-03-19 00:45:01 +0300

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Hello
+{
+    public class Hello
+    {
+        public void Massage()
+        {
+            Console.WriteLine("Здравствуйте, вы пользуетесь библиотекой студент:)");
+        }
+    }
+}
